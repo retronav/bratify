7becc32b612f3f63772eb678bfe6383f8fe0f3eb
Pranav <52596591+retronav@users.noreply.github.com>
2025-05-13 18:02:53 +0530

    Link to my personal website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 	<Generator />
 
 	<footer>
-		Made by <a href="https://github.com/retronav">retronav</a> xx.
+		Made by <a href="https://karawale.in">retronav</a> xx.
 		<a href="https://github.com/retronav/bratify">Source code</a>. Choose "custom color" in color
 		preset if your brat summer is giving a different color combo. <s>Scribbled</s> text only works when
 		you center your text first.
